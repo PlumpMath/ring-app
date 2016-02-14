15669a93231a5e4f79a75b3d29576cca212e1c09
Sava4 <sasha.savchenko@gmail.com>
2016-02-14 22:40:35 +0200

    compojure
    added compojure dep

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,6 +1,34 @@
-(ns ring-app.core)
+(ns ring-app.core
+  (:require [ring.adapter.jetty :as jetty]
+            [compojure.core :as compojure]
+            [ring.util.http-response :as response]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;(defn foo
+  ;"I don't do a whole lot."
+  ;[x]
+  ;(println x "Hello, World!"))
+
+
+(defn response-handler [request]
+  (response/ok
+    (str "<html><body> your IP is: "
+              (:remote-addr request)
+              "</body></html>")))
+
+(def handler
+  (compojure/routes
+    (compojure/GET "/" request response-handler)))
+
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
+
+(defn -main []
+  (jetty/run-jetty
+    (-> #'handler wrap-nocache wrap-reload)
+    {:port 3000
+     :join? false}))
+
